6f561551a914c4889ad3584e4aedd440595b8ca5
DAVID <Angel02ipvt@gmail.com>
2023-07-27 23:32:36 -0500

    ec3

---
+++ src/test/java/com/example/idat/ec3/Ec3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idat.ec3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
